d77bd28a4514a7892ae9b2116ccb7f7b04f74fb4
Rimack Zelnick <me@rzelnick.com>
2017-03-07 20:26:23 -0500

    See ChangeLog; Setup script code review

--- setup.php
+++ setup.php
@@ -1,23 +1,52 @@
+#!/usr/bin/env php
 <?php
-    define( 'DB_NAME',   'bd-cms'    );
+    /* Don't mess with my vim tag
+     * vim: number ts=4 sw=4 expandtab tw=76
+     */
+
+    define( 'DB_NAME',   'bdcms'     );
     define( 'DB_HOST',   'localhost' );
-    define( 'DB_UNAME',  'user'      );
-    define( 'DB_PASSWD', 'password'  );
+    define( 'DB_UNAME',  'root'      );
+    define( 'DB_PASSWD', ''          );
 
     try {
-        $pdo = new PDO( 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST,
+        /* connect */
+        $pdo = new PDO( 'mysql:host=' . DB_HOST,
             DB_UNAME, DB_PASSWD );
 
-        $st = $pdo->prepare( 'SHOW DATABASES',
-            array( PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL ) );
+        /* prepare statement & execute */
+        $stmt = $pdo->prepare( 'SHOW DATABASES' );
+        $stmt->execute( );
 
-        $st->execute( );
+        /* find our database */
+        echo 'Looking up database name ' . DB_NAME . "...\n";
 
-        while( $row = $st->fetch( ) )
-            print $row;
+        $db_found = false;
+        $db_list  = $stmt->fetchAll( PDO::FETCH_ASSOC );
+
+        for( $i = 0; $i < sizeof( $db_list ); ++$i ) {
+            if( ! strcmp( $db_list[ $i ][ "Database" ], DB_NAME ) )
+                $db_found = true;
+        }
+
+        /* if database not found then create one */
+        if( ! $db_found ) {
+            echo 'Creating database ' . DB_NAME . "...\n";
+
+            $stmt = $pdo->prepare( 'CREATE DATABASE ' . DB_NAME );
+            $stmt->execute( );
+
+            echo 'Database ' . DB_NAME . " created!\n";
+        }
+
+        /* database found */
+        echo 'Database ' . DB_NAME . " found!\n";
+        $stmt = $pdo->prepare( 'USE ' . DB_NAME );
+
+        /* now we need our database schema */
 
         $pdo = null;
     } catch( PDOException $ex ) {
-        echo 'Connection failed: ' . $ex->getMessage( );
+        echo 'Connection failed: ' . $ex->getMessage( ) . "\n";
     }
 ?>
