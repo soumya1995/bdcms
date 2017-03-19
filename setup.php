9d97c0c5207a4bb916762bad51460734586228c0
Rimack Zelnick <me@rzelnick.com>
2017-03-18 21:29:50 -0400

    See ChangeLog; setup script

--- setup.php
+++ setup.php
@@ -42,9 +42,78 @@
         /* database found */
         echo 'Database ' . DB_NAME . " found!\n";
         $stmt = $pdo->prepare( 'USE ' . DB_NAME );
+        $stmt->execute( );
 
         /* now we need our database schema */
-
+        echo "Create Tags Table\n";
+        $sql = <<<EOT
+            CREATE TABLE IF NOT EXISTS `Tags`(
+                `id` int(8),
+                `tagname` varchar(16),
+                PRIMARY KEY(`id`) );
+EOT;
+        $stmt = $pdo->prepare( $sql );
+        $stmt->execute( );
+        echo "Tags Table Created\n";
+        echo "Create Page Table\n";
+        $sql = <<<EOT
+            CREATE TABLE IF NOT EXISTS `Page`(
+                `id` int(8),
+                `title` varchar(32) NOT NULL,
+                `author_ids` varchar(32),
+                `content` varchar(4040),
+                PRIMARY KEY(`id`) );
+EOT;
+        $stmt = $pdo->prepare( $sql );
+        $stmt->execute( );
+        echo "Page Table Created\n";
+        echo "Create Users Table\n";
+        $sql = <<<EOT
+            CREATE TABLE IF NOT EXISTS `Users`(
+                `id` int(8),
+                `username` varchar(16),
+                `md5hash` varchar(16),
+                PRIMARY KEY(`id`) );
+EOT;
+        $stmt = $pdo->prepare( $sql );
+        $stmt->execute( );
+        echo "Users Table Created\n";
+        echo "Create Post Table\n";
+        $sql = <<<EOT
+            CREATE TABLE IF NOT EXISTS `Post`(
+                `id` int(8),
+                `title` varchar(32),
+                `category_id` varchar(12) NOT NULL,
+                `tags_ids` varchar(16),
+                `mmedia_ids` varchar(16),
+                `content` varchar(3984),
+                PRIMARY KEY(`id`) );
+EOT;
+        $stmt = $pdo->prepare( $sql );
+        $stmt->execute( );
+        echo "Post Table Created\n";
+        echo "Create Categories Table\n";
+        $sql = <<<EOT
+            CREATE TABLE IF NOT EXISTS `Categories`(
+                `id` int(8),
+                `category` varchar(24),
+                PRIMARY KEY(`id`) );
+EOT;
+        $stmt = $pdo->prepare( $sql );
+        $stmt->execute( );
+        echo "Categories Table Created\n";
+        echo "Create Multimedia Table\n";
+        $sql = <<<EOT
+            CREATE TABLE IF NOT EXISTS `Multimedia`(
+                `id` int(8),
+                `filecontent` LONGBLOB,
+                `filesize` unsigned(8),
+                PRIMARY KEY(`id`) );
+EOT;
+        $stmt = $pdo->prepare( $sql );
+        $stmt->execute( );
+        echo "Multimedia Table Created\n";
+        echo "Done setting up database schema configuration\n";
         $pdo = null;
     } catch( PDOException $ex ) {
         echo 'Connection failed: ' . $ex->getMessage( ) . "\n";
